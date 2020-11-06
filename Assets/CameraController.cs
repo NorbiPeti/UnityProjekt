b7a5715e34310f6f8c8b40e97b34271834a05f42
NorbiPeti <szatmari.norbert.peter@gmail.com>
2020-11-06 01:56:50 +0100

    Ground check, separate camera pos for diff directions

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,38 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 //  Class for following the player with the camera
 public class CameraController : MonoBehaviour {
     //  Camera speed
-    public float speed = 0.05f;
+    public float speed = 0.1f;
     //  GameObject to be followed.
     public Transform target;
     //  Our own transform
-    private Transform tr;
+    private Transform _tr;
 
     //  Center position of the target relative to the camera.
     public Vector3 offset;
+    private float _lastSwitch = -1;
+    private bool _facingRight;
 
     //  Store initial values
     void Start()
     {
-        tr = transform;
-        offset = target.position - tr.position;
+        _tr = transform;
+        offset = target.position - _tr.position;
     }
 
     //  Update positions
-    void FixedUpdate ()
+    void FixedUpdate()
     {
         if (!target) return;
         //  Get where the camera should be, and what movement is required.
-        var position = tr.position;
-        Vector3 anchorPos = position + offset;
+        var position = _tr.position;
+        bool facingLeft = target.localScale.x < 0;
+        Vector3 anchorPos = position + (facingLeft ? new Vector3(-offset.x, offset.y, offset.z) : offset);
         Vector3 movement = target.position - anchorPos;
 
+        float sp = speed;
+        /*if (_lastSwitch >= 0 && Time.time - _lastSwitch < 0.5f)
+            sp /= 4;*/
+        float diff = Time.time - _lastSwitch;
+        if (_lastSwitch >= 0 && diff < 2f)
+            sp *= 0.5f + diff / 4;
+
+        if (facingLeft == _facingRight) //Megváltozott az irány
+            _lastSwitch = Time.time;
+
+        _facingRight = !facingLeft;
+        
         //  Update position based on movement and speed.
-        Vector3 newCamPos = position + movement*speed;
+        Vector3 newCamPos = position + movement * sp;
         position = newCamPos;
-        tr.position = position;
+        _tr.position = position;
     }
 }
---
+++ Assets/OwnCharacterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class OwnCharacterController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Collider2D collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(rb.velocity.x) > 3)
+            return;
+        float input = Input.GetAxis("Horizontal");
+        if (input < 0 && rb.transform.localScale.x > 0
+            || input > 0 && rb.transform.localScale.x < 0)
+        {
+            var tr = transform;
+            var scale = tr.localScale;
+            scale.x *= -1;
+            tr.localScale = scale;
+        }
+        rb.AddForce(new Vector2(input * 5, 0));
+
+        var cols = new Collider2D[5];
+        /*if (Input.GetButtonDown("Jump") && rb.OverlapCollider(new ContactFilter2D(), cols) > 0
+                                        && cols.Any(col => col.CompareTag("Tiled")))*/
+        if (Input.GetButtonDown("Jump") && IsOnGround())
+            rb.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+    }
+
+    private bool IsOnGround()
+    {
+        var bounds = collider.bounds;
+        return Physics.CheckCapsule(bounds.center, new Vector3(bounds.center.x, bounds.min.y - 0.1f, bounds.center.z),
+            0.28f);
+    }
+}
